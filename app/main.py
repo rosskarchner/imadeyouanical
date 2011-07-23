86fdffcbdb23deb9a9f79769d65fc8234e3197e7
Ross Karchner <rosskarchner@gmail.com>
2011-07-22 21:03:38 -0400

    cleaning up main.py

--- app/main.py
+++ app/main.py
@@ -5,16 +5,11 @@ from google.appengine.ext.webapp import util
 debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 sys.path = ['lib', 'lib/dist', 'lib/dist.zip'] +sys.path
 import webapp2
-from utils import deserialize_entities, serialize_entities
 
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
 from google.appengine.api import memcache
-
-
-root_domain='shareevents.com'
-if debug:root_domain='shareevents.local:8080'
 
 
 
@@ -27,16 +22,10 @@ def enable_jinja2_debugging():
     HardenedModulesHook._WHITE_LIST_C_MODULES += ['_ctypes', 'gestalt']
 
 
-root_app  = webapp2.WSGIApplication([
-    ('/provision', 'provision.ProvisionHandler'),
 
-], debug=debug)
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', 'views.IndexHandler'),
 
-calendar_app = webapp2.WSGIApplication([
-    webapp2.Route('/', 'se_calendar.FrontPageHandler'),
-     webapp2.Route('/config', 'se_calendar.ConfigurationRegistryHandler'),
-     webapp2.Route('/add', 'se_calendar.SubmitEventHandler'),
-    webapp2.Route('/setup/<guid>','provision.DeployHandler'),
 ], debug=debug)
 
 
@@ -44,62 +33,9 @@ calendar_app = webapp2.WSGIApplication([
 enable_jinja2_debugging()
 
 
-# "virtual hosting" for calendars
-def calendar_host(environ, start_response):
-    from models import EditedCalendar
-    key=environ['HTTP_HOST']
-    calendar = deserialize_entities(memcache.get('calendar:'+key))
-    if not calendar:
-        calendar= EditedCalendar.get_by_key_name(key)
-        if calendar: memcache.set('calendar:'+key, serialize_entities(calendar))
-        
-    
-    if calendar:
-        environ['calendar'] = calendar
-        return calendar_app(environ, start_response)
-    else:
-        response_body = ""
-        status = '302 Redirect'
-        if root_domain in environ['HTTP_HOST']:
-            response_headers = [('Location', 'http://www.%s/provision?domain=%s' % (root_domain,environ['HTTP_HOST'].split('.')[0])),
-                  ]
-        else:
-            response_headers = [('Location', 'http://www.%s/provision' % root_domain),
-                  ]
-        
-        
-        start_response(status, response_headers)
-        return response_body
-
-
-def default_host(environ, start_response):
-        response_body = ""
-        status = '302 Redirect'
-        response_headers = [('Location', 'http://www.%s/' % root_domain),
-                  ]
-        start_response(status, response_headers)
-        return response_body
-
-    
-    
-
-    
-
-
-# set up domain routing
-import wfront
-
-mapping = [
-            ('www.'+root_domain, root_app, None),
-           ('*.'+root_domain, calendar_host, None),
-           ]
-
-router = wfront.route(mapping, default=default_host)
-
 
 def main():
-    util.run_wsgi_app(router)
-
+    app.run()
 
 if __name__ == '__main__':
     main()
